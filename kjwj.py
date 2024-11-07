4175298f45139a77d1155086338ca30a50007a60
似曾相识 <48168591+sicxsi@users.noreply.github.com>
2024-11-07 15:09:40 +0800

    Add files via upload

--- kjwj.py
+++ kjwj.py
@@ -1,14 +1,13 @@
 # name: 科技玩家
 # Author: sicxs
 # Date: 2024-11-4
-# export kjwj="authorization" @,&分割
+# export kjwj="authorization" @,&分割 不需要 Bearer 
 # cron: 15 8 * * *
 # new Env('科技玩家');
 import requests
 import re,os,sys,json,time
 
 
-authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd3d3Lmtlaml3YW5qaWEubmV0IiwiaWF0IjoxNzMwNzI3Njg0LCJuYmYiOjE3MzA3Mjc2ODQsImV4cCI6MTczMTkzNzI4NCwiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiODM0OCJ9fX0.OMOy5g3Nm4UkH-zE2IlJm4XJe41n3QIJA7tE9-QjAfA"
 def getUserInfo(authorization):#我的信息
     url = "https://www.kejiwanjia.net/wp-json/b2/v1/getUserInfo"
     header = {
@@ -16,7 +15,7 @@ def getUserInfo(authorization):#我的信息
      "method": "POST",
      "path": "/wp-json/b2/v1/getUserInfo",
      "scheme": "https",
-     "authorization": authorization,
+     "authorization": f"Bearer {authorization}",
      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
      "origin": "https://www.kejiwanjia.net",
      "referer": "https://www.kejiwanjia.net/",
@@ -26,6 +25,8 @@ def getUserInfo(authorization):#我的信息
         response.encoding = "utf-8"
         info = json.loads(response.text)
         print(f"用户名: {info['user_data']['name']}")
+        time.sleep(3)
+        getUserMission(authorization)
         time.sleep(5)
         userMission(authorization)  
     except Exception as e:
@@ -38,7 +39,7 @@ def getGoldList(authorization):#积分查询
      "method": "POST",
      "path": "/wp-json/b2/v1/getGoldList",
      "scheme": "https",
-     "authorization": authorization,
+     "authorization": f"Bearer {authorization}",
      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
      "origin": "https://www.kejiwanjia.net",
      "referer": "https://www.kejiwanjia.net/",
@@ -51,6 +52,25 @@ def getGoldList(authorization):#积分查询
     except Exception as e:
           print(e)
 
+def getUserMission(authorization):#更新签到
+    url = "https://www.kejiwanjia.net/wp-json/b2/v1/getUserMission"
+    header = {
+     "authority": "www.kejiwanjia.net",
+     "method": "POST",
+     "path": "/wp-json/b2/v1/getUserMission",
+     "scheme": "https",
+     "authorization": f"Bearer {authorization}",
+     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+     "origin": "https://www.kejiwanjia.net",
+     "referer": "https://www.kejiwanjia.net/",
+        }
+    try:
+        response = requests.post(url=url,headers=header)
+        s1 = response.status_code 
+        if 200 ==s1:
+            print("正在初始签到，请稍等。")
+    except Exception as e:
+          print(e)
 def userMission(authorization): #签到
     url = "https://www.kejiwanjia.net/wp-json/b2/v1/userMission"
     header = {
@@ -58,7 +78,7 @@ def userMission(authorization): #签到
      "method": "POST",
      "path": "/wp-json/b2/v1/userMission",
      "scheme": "https",
-     "authorization": authorization,
+     "authorization": f"Bearer {authorization}",
      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
      "origin": "https://www.kejiwanjia.net",
      "referer": "https://www.kejiwanjia.net/",
@@ -70,13 +90,13 @@ def userMission(authorization): #签到
         if "message" in  info :
           print("你的authorization可能过期了，请检查。")
         elif "credit" in info:
-          print(f"签到获得；{info['credit']}")
+          print(f"恭喜您，签到获得,{info['credit']}积分")
           time.sleep(5)
           getGoldList(authorization)
         else:
             print("请勿重复签到")
             time.sleep(5)
-            getGoldList(authorization)  
+           # getGoldList(authorization)  
          #print(f"最近一次签到：{info['data'][0]['date']},获得积分；{info['data'][0]['no']},总积分：{info['data'][0]['total']}")
     except Exception as e:
           print(e)
@@ -104,6 +124,4 @@ def sicxs():
     print(f'\n-----------  执 行  结 束 -----------')
  
 if __name__ == '__main__':
-
- getUserInfo(authorization)      
- sicxs()
+  sicxs()
